d00a1100bceeb02e97cf15bc0226f9caeb2524d6
shubhash kunwar <subashkunwar94@gmail.com>
2023-10-18 18:03:20 -0700

    Adding captain.py file

---
+++ captain.py
@@ -0,0 +1,9 @@
+class Captain():
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def captain_info(self):
+        print(f'the captain name is {self.name} and he is {self.age} years old')
+
+        
